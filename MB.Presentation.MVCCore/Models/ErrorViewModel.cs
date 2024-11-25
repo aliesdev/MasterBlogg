ce8b5e90be9e636700c31f07b6b7cedb843c2bfb
aliesdev <ali.esmaeilpour.eng@gmail.com>
2024-11-25 13:25:29 +0330

    Deleted empty folder

--- MB.Presentation.MVCCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MB.Presentation.MVCCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
